f5227457f6c4552414cef5ce5bd522480fdb0ce3
Simon H <github@pointbeing.net>
2024-06-30 22:56:43 +0100

    Enable Django auth URLs eg. login/out

--- tests/urls.py
+++ tests/urls.py
@@ -3,5 +3,6 @@ from django.urls import include, path
 # the app urls.py must be include()d to pick up the route namespace ie 'djwiki'
 
 urlpatterns = [
+    path('accounts/', include('django.contrib.auth.urls')),
     path('', include('wikiwikiweb.urls')),
 ]
